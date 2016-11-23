aa555791a2921731b696b19543ddbb85cf78c678
seonggu.baek <seonggu.baek@nexstreaming.com>
2016-11-23 15:02:27 +0900

    04. Implementing Account Authentication

--- server/main.js
+++ server/main.js
@@ -1,16 +1,47 @@
-import WebpackDevServer from 'webpack-dev-server';
-import webpack from 'webpack';
-
-const devPort = 4000;
-
-// Express Code
 import express from 'express';
 import path from 'path';
 
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
+import morgan from 'morgan';            // HTTP REQUEST LOGGER
+import bodyparser from 'body-parser';   // PARSE HTML BODY
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+/* setup routers & static directory */
+import api from './routes';
+
 const app = express();
 const port = 3000;
+const devPort = 4000;
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+mongoose.connect('mongodb://test:password@127.0.0.1:27017/database=');
+//mongoose.connect('mongodb://localhost/codelab');
 
 app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+
+/* use session */
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/api', api);
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.get('/hello', (req, res) => {
     return res.send('Hello CodeLab');
@@ -19,11 +50,10 @@ app.get('/hello', (req, res) => {
 app.listen(port, () => {
     console.log('Express is listening on port', port);
 });
-// Express Code
 
-if (process.env.NODE_ENV == 'development') {
+if(process.env.NODE_ENV == 'development') {
     console.log('Server is running on development mode');
-    const config = require('../webpack.dev.config.');
+    const config = require('../webpack.dev.config');
     const compiler = webpack(config);
     const devServer = new WebpackDevServer(compiler, config.devServer);
     devServer.listen(
